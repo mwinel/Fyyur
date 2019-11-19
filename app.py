82c50f1a982ce04eb46affdef923050f91908978
Nelson Mwirumubi <mwinel@live.com>
2019-11-19 13:13:15 +0300

    Add fetch artists implementation

--- app.py
+++ app.py
@@ -224,17 +224,8 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    artists = Artist.query.all()
+    data = [i for i in artists]
     return render_template('pages/artists.html', artists=data)
 
 
