6c491c09e601faa4472674b8349095847a2d2d52
Nelson Mwirumubi <mwinel@live.com>
2019-11-19 13:26:46 +0300

    Add artist search implementation

--- app.py
+++ app.py
@@ -231,16 +231,24 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+    data = []
+    artists = []
+    search_term = request.form.get("search_term").lower()
+    for artist in db.session.query(Artist).with_entities(Artist.id, Artist.name).all():
+        if search_term in artist.name.lower():
+            artists.append(artist)
+    for artist in artists:
+        # Filter artist upcoming shows
+        upcoming_shows = Show.query.filter_by(artist_id=artist.id).filter(
+            Show.start_time > datetime.utcnow().isoformat()).all()
+        data.append({
+            "id": artist.id,
+            "name": artist.name,
+            "num_upcoming_shows": len(upcoming_shows)
+        })
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(artists),
+        "data": data
     }
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
